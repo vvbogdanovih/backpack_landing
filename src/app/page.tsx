25f378eafdacbdd27f11611576a836455d47534c
vvbogdanovih <vvbogdanovih@gmail.com>
2025-04-09 11:41:26 +0300

    Initial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,57 @@
-import Image from "next/image";
-
+'use client'
+import Image from 'next/image'
+import toast from 'react-hot-toast'
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+	return (
+		<div>
+			<section className='relative w-full h-screen overflow-hidden'>
+				{/* Відео */}
+				<video
+					src='\video_248741029.mp4'
+					autoPlay
+					loop
+					muted
+					playsInline
+					className='absolute top-0 left-0 w-full h-full object-cover z-0'
+				></video>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+				{/* Напівпрозора затемнююча підкладка */}
+				<div className='absolute inset-0 bg-[#0000009c] z-10'></div>
+
+				{/* Текст посередині */}
+				<div className='absolute inset-0 flex items-center justify-center z-20'>
+					<div className='text-center text-white px-6'>
+						<h1 className='text-5xl md:text-9xl font-bold mb-4'>
+							Рюкзак для міста
+						</h1>
+						<p className='text-lg md:text-5xl'>
+							Функціональність. Стиль. Надійність.
+						</p>
+					</div>
+				</div>
+			</section>
+
+			<div>
+				<div className='flex items-center justify-between'>
+					<div className='text-center'>
+						<p className='text-7xl font-bold'>
+							9 з 9 бобрів рекомендують цей рюкзак
+						</p>
+						<p className='text-5xl text-gray-400 mt-1'>
+							Такий носить навіть Стетхем
+						</p>
+					</div>
+					<div>
+						<Image
+							src='/photos/download1.png'
+							alt='Рюкзак'
+							width={1200}
+							height={900}
+							className='h-screen'
+						/>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
 }
